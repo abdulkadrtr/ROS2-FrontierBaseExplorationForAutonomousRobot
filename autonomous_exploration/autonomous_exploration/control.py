9e274ca75318a3f0a6b25aa29b6b6319072811d0
Abdulkadir TÜRE <87595266+abdulkadrtr@users.noreply.github.com>
2023-02-19 14:28:23 +0300

    Update control.py
    The code has undergone several optimizations and changes to its algorithms.

--- autonomous_exploration/autonomous_exploration/control.py
+++ autonomous_exploration/autonomous_exploration/control.py
@@ -1,12 +1,14 @@
 import rclpy
 from rclpy.node import Node
+from nav_msgs.msg import OccupancyGrid , Odometry
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 import numpy as np
 import heapq
-from nav_msgs.msg import OccupancyGrid , Odometry
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
 import math
 import scipy.interpolate as si
+import sys
+import random
 import yaml
 
 with open("src/autonomous_exploration/config/params.yaml", 'r') as file:
@@ -15,9 +17,9 @@ with open("src/autonomous_exploration/config/params.yaml", 'r') as file:
 lookahead_distance = params["lookahead_distance"]
 speed = params["speed"]
 expansion_size = params["expansion_size"]
-group_length = params["group_length"]
 target_error = params["target_error"]
 robot_r = params["robot_r"]
+
 
 def euler_from_quaternion(x,y,z,w):
     t0 = +2.0 * (w * x + y * z)
@@ -36,104 +38,88 @@ def heuristic(a, b):
     return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
 
 def astar(array, start, goal):
-
     neighbors = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
-
     close_set = set()
-
     came_from = {}
-
     gscore = {start:0}
-
     fscore = {start:heuristic(start, goal)}
-
     oheap = []
-
     heapq.heappush(oheap, (fscore[start], start))
-
-
     while oheap:
-
         current = heapq.heappop(oheap)[1]
-
         if current == goal:
-
             data = []
-
             while current in came_from:
-
                 data.append(current)
-
                 current = came_from[current]
-
+            data = data + [start]
+            data = data[::-1]
             return data
-
         close_set.add(current)
-
         for i, j in neighbors:
-
             neighbor = current[0] + i, current[1] + j
-
             tentative_g_score = gscore[current] + heuristic(current, neighbor)
-
             if 0 <= neighbor[0] < array.shape[0]:
-
                 if 0 <= neighbor[1] < array.shape[1]:                
-
                     if array[neighbor[0]][neighbor[1]] == 1:
-
                         continue
-
                 else:
-
                     # array bound y walls
-
                     continue
-
             else:
-
                 # array bound x walls
-
                 continue
-
-
             if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
-
                 continue
-
-
             if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
-
                 came_from[neighbor] = current
-
                 gscore[neighbor] = tentative_g_score
-
                 fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
-
                 heapq.heappush(oheap, (fscore[neighbor], neighbor))
-
-
+    # If no path to goal was found, return closest path to goal
+    if goal not in came_from:
+        closest_node = None
+        closest_dist = float('inf')
+        for node in close_set:
+            dist = heuristic(node, goal)
+            if dist < closest_dist:
+                closest_node = node
+                closest_dist = dist
+        if closest_node is not None:
+            data = []
+            while closest_node in came_from:
+                data.append(closest_node)
+                closest_node = came_from[closest_node]
+            data = data + [start]
+            data = data[::-1]
+            return data
     return False
 
-def bspline_planning(x, y, sn):
-    N = 2
-    t = range(len(x))
-    x_tup = si.splrep(t, x, k=N)
-    y_tup = si.splrep(t, y, k=N)
+def bspline_planning(array, sn):
+    try:
+        array = np.array(array)
+        x = array[:, 0]
+        y = array[:, 1]
+        N = 2
+        t = range(len(x))
+        x_tup = si.splrep(t, x, k=N)
+        y_tup = si.splrep(t, y, k=N)
 
-    x_list = list(x_tup)
-    xl = x.tolist()
-    x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
+        x_list = list(x_tup)
+        xl = x.tolist()
+        x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
 
-    y_list = list(y_tup)
-    yl = y.tolist()
-    y_list[1] = yl + [0.0, 0.0, 0.0, 0.0]
+        y_list = list(y_tup)
+        yl = y.tolist()
+        y_list[1] = yl + [0.0, 0.0, 0.0, 0.0]
 
-    ipl_t = np.linspace(0.0, len(x) - 1, sn)
-    rx = si.splev(ipl_t, x_list)
-    ry = si.splev(ipl_t, y_list)
-
-    return rx, ry
+        ipl_t = np.linspace(0.0, len(x) - 1, sn)
+        rx = si.splev(ipl_t, x_list)
+        ry = si.splev(ipl_t, y_list)
+        path = [(rx[i],ry[i]) for i in range(len(rx))]
+    except:
+        path = array
+    return path
 
 def pure_pursuit(current_x, current_y, current_heading, path,lookahead_distance,index):
     closest_point = None
@@ -207,32 +193,46 @@ def dfs(matrix, i, j, group, groups):
     return group + 1
 
 def deleteGroups(groups):
-    for group in list(groups):
-        if len(groups[group]) <= group_length:
-            del groups[group]
+    sorted_groups = sorted(groups.items(), key=lambda x: len(x[1]), reverse=True)
+    top_five_groups = sorted_groups[:5]
+    if len(sorted_groups) > 5:
+        for group in sorted_groups[5:]:
+            if group not in top_five_groups:
+                del groups[group[0]] 
     return groups
 
+def calculate_centroid(x_coords, y_coords):
+    n = len(x_coords)
+    sum_x = sum(x_coords)
+    sum_y = sum(y_coords)
+    mean_x = sum_x / n
+    mean_y = sum_y / n
+    centroid = (int(mean_x), int(mean_y))
+    return centroid
+
 def findClosestGroup(matrix,groups, current,resolution,originX,originY):
-    min = 10000000
+    min = 1000
     target = None
     for value in groups.items():
-        middle = value[1][int(len(value[1])/2)]
+        middle = calculate_centroid([p[0] for p in value[1]],[p[1] for p in value[1]])
         path = astar(matrix, current, middle)
-        if path == False:
-            continue
         path = [(p[1]*resolution+originX,p[0]*resolution+originY) for p in path]
-        for i in range(len(path)):
-            path[i] = (path[i][0],path[i][1])
-            points = np.array(path)
-        differences = np.diff(points, axis=0)
-        distances = np.hypot(differences[:,0], differences[:,1])
-        total_distance = np.sum(distances)
+        total_distance = pathLength(path)
         if total_distance < min and total_distance > target_error*2:
             target = middle
             min = total_distance
         if target == None:
-            target = value[1][-1]
+            target = value[1][random.randint(0,len(value[1])-1)]
     return target[0],target[1]
+
+def pathLength(path):
+    for i in range(len(path)):
+        path[i] = (path[i][0],path[i][1])
+        points = np.array(path)
+    differences = np.diff(points, axis=0)
+    distances = np.hypot(differences[:,0], differences[:,1])
+    total_distance = np.sum(distances)
+    return total_distance
 
 def costmap(data,width,height,resolution):
     data = np.array(data).reshape(height,width)
@@ -249,6 +249,43 @@ def costmap(data,width,height,resolution):
     data = data*resolution
     return data
 
+def exploration(data,width,height,resolution,column,row,originX,originY):
+        data = costmap(data,width,height,resolution)
+        data[row][column] = 0 #Robot Anlık Konum
+        data[data > 5] = 1 # 0 olanlar gidilebilir yer, 100 olanlar kesin engel
+        data = frontierB(data)
+        data,groups = assign_groups(data)
+        print("Grup Sayısı: ",len(groups))
+        groups = deleteGroups(groups)
+        print("Grup Sayısı: ",len(groups))
+        if len(groups) == 0:
+            print("[BILGI] KESIF TAMAMLANDI")
+            sys.exit()
+        data[data < 0] = 1 #-0.05 olanlar bilinmeyen yer
+        #data icerigi 0: gidilebilir, 1: engel
+        rowH,columnH = findClosestGroup(data,groups,(row,column),resolution,originX,originY)
+        path = astar(data,(row,column),(rowH,columnH))
+        path = [(p[1]*resolution+originX,p[0]*resolution+originY) for p in path]
+        path = bspline_planning(path,len(path)*5)
+        return path
+
+def localControl(scan):
+    v = None
+    w = None
+    for i in range(60):
+        if scan[i] < robot_r:
+            v = 0.2
+            w = -math.pi/4 
+            break
+    if v == None:
+        for i in range(300,360):
+            if scan[i] < robot_r:
+                v = 0.2
+                w = math.pi/4
+                break
+    return v,w
+
+
 class navigationControl(Node):
     def __init__(self):
         super().__init__('Exploration')
@@ -259,46 +296,25 @@ class navigationControl(Node):
         timer_period = 0.01
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.flag = 1
-        print("[BILGI] KESIF MODU AKTIF")
-        print("[BILGI] YENI HEDEF BELIRLENIYOR")
+        print("[BILGI] KESİF MODU AKTİF")
+        print("[BILGI] YENI HEDEF BELİRLENİYOR")
 
     def scan_callback(self,msg):
         self.scan = msg.ranges
 
     def listener_callback(self,msg):
-        if self.flag == 1:
+        if (self.x in locals()) == False:
             self.resolution = msg.info.resolution
             self.originX = msg.info.origin.position.x
             self.originY = msg.info.origin.position.y
-            if self.x == None:
-                return
-            column = int((self.x- msg.info.origin.position.x)/msg.info.resolution)
-            row = int((self.y- msg.info.origin.position.y)/msg.info.resolution)
-            width = msg.info.width
-            height = msg.info.height
-            data = costmap(msg.data,width,height,msg.info.resolution)
-            data[row][column] = 0 #Robot Anlık Konum
-            data[data > 5] = 1 # 0 olanlar gidilebilir yer, 100 olanlar kesin engel
-            data = frontierB(data)
-            data,groups = assign_groups(data)
-            groups = deleteGroups(groups)
-            if len(groups) == 0:
-                print("[BILGI] KESIF TAMAMLANDI")
-                self.flag = 3
-                return
-            data[data < 0] = 1 #-0.05 olanlar bilinmeyen yer
-            #data icerigi 0: gidilebilir, 1: engel
-            rowH,columnH = findClosestGroup(data,groups,(row,column),self.resolution,self.originX,self.originY)
-            path = astar(data,(row,column),(rowH,columnH))
-            path = path + [(row,column)]
-            path = path[::-1]
-            pathB = path
-            pathB = [(p[1]*self.resolution+self.originX,p[0]*self.resolution+self.originY) for p in pathB]
-            pathB = np.array(pathB)
-            pathX = pathB[:,0]
-            pathY = pathB[:,1]
-            pathX,pathY = bspline_planning(pathX,pathY,len(pathX)*5)
-            self.path = [(pathX[i],pathY[i]) for i in range(len(pathX))]
+            self.width = msg.info.width
+            self.height = msg.info.height
+            self.data = msg.data
+            self.column = int((self.x- msg.info.origin.position.x)/msg.info.resolution)
+            self.row = int((self.y- msg.info.origin.position.y)/msg.info.resolution)
+
+        if self.flag == 1:
+            self.path = exploration(self.data,self.width,self.height,self.resolution,self.column,self.row,self.originX,self.originY)
             print("[BILGI] HEDEFE GIDILIYOR..")
             self.i = 0
             self.flag = 2
@@ -306,36 +322,26 @@ class navigationControl(Node):
     def timer_callback(self):
         if self.flag == 2:
             twist = Twist()
-            for i in range(60):
-                if self.scan[i] < robot_r:
-                    twist.linear.x = 0.2
-                    twist.angular.z = -math.pi/4
-                    self.publisher.publish(twist)
-                    print("[BILGI] LOKAL ENGEL TESPİT EDİLDİ")
-                    return
-            for i in range(300,360):
-                if self.scan[i] < robot_r:
-                    twist.linear.x = 0.2
-                    twist.angular.z = math.pi/4
-                    self.publisher.publish(twist)
-                    print("[BILGI] LOKAL ENGEL TESPİT EDİLDİ")
-                    return
-            twist.linear.x , twist.angular.z,self.i = pure_pursuit(self.x,self.y,self.yaw,self.path,lookahead_distance,self.i)
+            v , w = localControl(self.scan)
+            if v == None:
+                v, w,self.i = pure_pursuit(self.x,self.y,self.yaw,self.path,lookahead_distance,self.i)
+            else:
+                print("[BILGI] ENGEL TESPİT EDİLDİ]")
             if(abs(self.x - self.path[-1][0]) < target_error and abs(self.y - self.path[-1][1]) < target_error):
-                twist.linear.x = 0.0
-                twist.angular.z = 0.0
+                v = 0.0
+                w = 0.0
                 self.flag = 1
                 print("[BILGI] HEDEFE ULASILDI")
-                print("[BILGI] YENI HEDEF BELIRLENIYOR")
+                print("[BILGI] YENI HEDEF BELİRLENİYOR")
+            twist.linear.x = v
+            twist.angular.z = w
             self.publisher.publish(twist)
-
 
     def info_callback(self,msg):
         self.x = msg.pose.pose.position.x
         self.y = msg.pose.pose.position.y
         self.yaw = euler_from_quaternion(msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,
         msg.pose.pose.orientation.z,msg.pose.pose.orientation.w)
-
 
 
 def main(args=None):
@@ -347,4 +353,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
